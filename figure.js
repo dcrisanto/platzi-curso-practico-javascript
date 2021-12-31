8e5cce262cbb5c1568d4358d2cbadb06e2ddbbc0
dcrisanto <dorelly.crisanto@gmail.com>
2021-12-30 21:07:16 -0500

    Calculando perímetro y área de triángulo

--- figure.js
+++ figure.js
@@ -1,3 +1,5 @@
+// Cuadrado
+
 const squareSide = document.getElementById('side');
 
 const buttonPerimeter = document.getElementById('perimeter');
@@ -16,5 +18,26 @@ buttonArea.addEventListener('click', resultArea = () => {
     result.innerHTML = "El área del cuadrado es: " + calculatingArea; 
 })
 
+// Triángulo
 
+const triangleSide1 = document.getElementById('triangleSide1');
+const triangleSide2 = document.getElementById('triangleSide2');
+const triangleBase = document.getElementById('triangleBase');
+const triangleHeight = document.getElementById('triangleHeight')
+
+const buttontrianglePerimeter = document.getElementById('trianglePerimeter');
+
+const buttontriangleArea = document.getElementById('triangleArea');
+
+const triangleResult = document.getElementById('triangleResult');
+
+buttontrianglePerimeter.addEventListener('click', trianglePerimeter = () => {
+    const calculatingPerimeter = parseInt(triangleSide1.value) + parseInt(triangleSide2.value) + parseInt(triangleBase.value);
+    triangleResult.innerHTML = "El perímetro del triángulo es: " + calculatingPerimeter;
+})
+
+buttontriangleArea.addEventListener('click', triangleArea = () => {
+    const calculatingArea = parseInt(triangleBase.value)*parseInt(triangleHeight.value)/2;
+    triangleResult.innerHTML = "El área del triángulo es: " + calculatingArea;
+})
 
